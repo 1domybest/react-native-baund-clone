ab61abdce364436d9fc0c97921055ea7837947c4
1domybest <72382663+1domybest@users.noreply.github.com>
2023-01-11 10:10:27 +0900

    1
    1

--- src/constants/ConstCode.js
+++ src/constants/ConstCode.js
@@ -4,7 +4,7 @@ const AWS_CODE = {
     AWSbucketRegion: 'ap-northeast-2', // 따로 숨겨야함
     AWSIdentityPoolId: 'ap-northeast-2:9b40e9d4-35d3-4d21-8a13-317c4151c1e5', // 따로 숨겨야함,
     AWSAccessKey: 'AKIAUO2TPNPMF6Z5QO76',
-    AWSSecretAccessKey: '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'
+    AWSSecretAccessKey: '/qOez4BE83Cy2ZOa1QpX5u8FNlbC5fCvE9KcdblH' 
   }
 
  export const ConstCode = {    
