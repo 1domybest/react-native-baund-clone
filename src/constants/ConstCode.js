b62ae0dea60f30b424f4bac282d90be637a5a54e
1domybest <72382663+1domybest@users.noreply.github.com>
2023-01-11 10:06:25 +0900

    1
    1

--- src/constants/ConstCode.js
+++ src/constants/ConstCode.js
@@ -2,9 +2,9 @@
 const AWS_CODE = {
     AWSalbumBucketName: 'baund', // 따로 숨겨야함
     AWSbucketRegion: 'ap-northeast-2', // 따로 숨겨야함
-    AWSIdentityPoolId: 'ap-northeast-2:9b40e9d4-35d3-4d21-8a13-317c4151c1e5', // 따로 숨겨야함,
-    AWSAccessKey: 'AKIAUO2TPNPMF6Z5QO76',
-    AWSSecretAccessKey: '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'
+    AWSIdentityPoolId: '', // 따로 숨겨야함,
+    AWSAccessKey: '',
+    AWSSecretAccessKey: ''
   }
 
  export const ConstCode = {    
