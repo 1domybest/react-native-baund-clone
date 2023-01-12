1b9c847cd2b42abcd0beb426e02fa772958c845b
1domybest <72382663+1domybest@users.noreply.github.com>
2023-01-12 17:15:03 +0900

    간단한 로그아웃 화면 구현
    간단한 로그아웃 화면 구현

--- src/screens/main/ProfileScreen.js
+++ src/screens/main/ProfileScreen.js
@@ -2,11 +2,21 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { makeStyles } from '@rneui/themed';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native'
+import { Button } from '@rneui/base';
+import * as $Util from '../../constants/utils'
+import {ROUTES} from '../../constants/routes'
 const ProfileScreen = () => {
+    const navigation = useNavigation();
+    const logOut = () => {
+        $Util.removeStoreData('token');
+        navigation.replace(ROUTES.INDEX)
+    }
     return (
         <SafeAreaView>
             <View>
                 <Text>프로필</Text>
+                <Button onPress={() => logOut()}>로그아웃</Button>
             </View>
         </SafeAreaView>
     )
